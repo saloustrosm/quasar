d3698bebdc98e3a933a40b4cf7c5266c18cf8d14
saloustrosm <saloustrosm@gmail.com>
2017-09-05 10:05:39 +0300

    Greek translation
    Greek Translation

---
+++ src/i18n/gr.js
@@ -0,0 +1,22 @@
+export const i18n = {
+  dayNames: 'Κυριακή_Δευτέρα_Τρίτη_Τετάρτη_Πέμπτη_Παρασκευή_Σάββατο'.split('_'),
+  monthNames: 'Ιανουάριος_Φεβρουάριος_Μάρτιος_Απρίλιος_Μάιος_Ιούνιος_Ιούλιος_Αύγουστος_Σεπτέμβριος_Οκτώβριος_Νοέμβριος_Δεκέμβριος'.split('_'),
+  clearLabel: 'Εκκαθάριση',
+  okLabel: 'Όρισε',
+  cancelLabel: 'Άκυρο',
+  selectLabel: 'Επιλογή',
+  dataTable: {
+    columns: 'Στήλες',
+    allCols: 'Όλες οι Στήλες',
+    rows: 'Γραμμές',
+    selected: {
+      singular: 'πεδίο επιλέχθηκε.',
+      plural: 'πεδία επιλέχθηκαν.'
+    },
+    clear: 'Εκκαθάριση',
+    search: 'Αναζήτηση',
+    all: 'Όλα',
+    noDataAfterFiltering: '<i class="material-icons">warning</i> Κανένα αποτέλεσμα. Παρακαλώ ελέγξτε τα κριτήρια αναζήτησης.',
+    noData: '<i class="material-icons">warning</i> Δεν βρέθηκαν αποτελέσματα προς εμφάνιση.'
+  }
+}
